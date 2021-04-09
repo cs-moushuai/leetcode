29d18f07e78a30054aba0ac9b69bca535dc78744
little-boy52 <1144612318@qq.com>
2021-04-09 20:40:26 +0800

    update

--- ex_647.cpp
+++ ex_647.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <vector>
 
 using namespace std;
 
@@ -7,6 +8,46 @@ class Solution
 public:
     int countSubstrings(string s)
     {
+        int len = s.size();
+        int ans = 0;
+        for (int center = 0; center < len * 2 + 1; ++center)
+        {
+            int left = center / 2;
+            int right = left + center % 2;
+            while (left >= 0 && right < len && s[left] == s[right])
+            {
+                --left;
+                ++right;
+                ++ans;
+            }
+
+        }
+        return ans;
+    }
+};
+
+
+class Solution2
+{
+public:
+    int countSubstrings(string s)
+    {
+        int len = s.size();
+        vector<vector<int>> dp(len, vector<int>(len, 0));
+        int ans = 0;
+        for (int j = 0; j < len; ++j)
+        {
+            for (int i = 0; i <= j; ++i)
+            {
+                if (s[i] == s[j] && (j - i + 1 <= 2 || dp[i + 1][j - 1]))
+                {
+                    dp[i][j] = true;
+                    ++ans;
+                }
+
+            }
+        }
+        return ans;
 
     }
 };
